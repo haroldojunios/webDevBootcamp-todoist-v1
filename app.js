756ac7d2621edbfed4c3768e7b9cc5fe6d5773a6
Haroldo Júnio <haroldojunios@gmail.com>
2021-12-05 19:11:26 -0300

    Aula 5
    Escopo

--- app.js
+++ app.js
@@ -9,20 +9,20 @@ app.set("view engine", "ejs");
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
-var items = ["Buy food", "Cook food", "Eat food"];
+let items = ["Buy food", "Cook food", "Eat food"];
 
 app.get("/", function (req, res) {
-  var today = new Date();
+  let today = new Date();
 
-  var options = { weekday: "long", day: "numeric", month: "long" };
+  const options = { weekday: "long", day: "numeric", month: "long" };
 
-  var day = today.toLocaleDateString("en-US", options);
+  let day = today.toLocaleDateString("en-US", options);
 
   res.render("list", { kindOfDay: day, newListItems: items });
 });
 
 app.post("/", function (req, res) {
-  var item = req.body.newItem;
+  let item = req.body.newItem;
   items.push(item);
   res.redirect("/");
 });
