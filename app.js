c5a5b8ada2fd8a9f284fca817d79a16e89775024
Haroldo Júnio <haroldojunios@gmail.com>
2021-12-05 20:05:46 -0300

    Aula 7
    EJS layouts

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 let items = ["Buy food", "Cook food", "Eat food"];
+let workItens = [];
 
 app.get("/", function (req, res) {
   let today = new Date();
@@ -19,13 +20,26 @@ app.get("/", function (req, res) {
 
   let day = today.toLocaleDateString("en-US", options);
 
-  res.render("list", { kindOfDay: day, newListItems: items });
+  res.render("list", { listTitle: day, newListItems: items });
 });
 
 app.post("/", function (req, res) {
   let item = req.body.newItem;
-  items.push(item);
-  res.redirect("/");
+  if (req.body.list === "Work") {
+    workItens.push(item);
+    res.redirect("/work");
+  } else {
+    items.push(item);
+    res.redirect("/");
+  }
+});
+
+app.get("/work", function (req, res) {
+  res.render("list", { listTitle: "Work", newListItems: workItens });
+});
+
+app.get("/about", function (req, res) {
+  res.render("about");
 });
 
 app.listen(3000, function () {
